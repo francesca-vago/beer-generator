d1e646aa6ed7eb99e3c86ade1884e5d696a74e28
Francesca Vago <vago.francesca@gmail.com>
2020-10-29 15:34:41 +0100

    Created react app for beer generator and fetch call from PunkAPI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [randomBeer, setRandomBeer] = useState({});
+
+  useEffect(() => {
+    axios
+      .get('https://api.punkapi.com/v2/beers/random')
+      .then(response => setRandomBeer(response.data[0]))
+      .catch(error => console.log(error));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Beer Generator</h1>
+      <p>{randomBeer.name}</p>
+      <p>{randomBeer.tagline}</p>
+      <p>{randomBeer.description}</p>
     </div>
   );
 }
